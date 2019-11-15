a6624debf74c410d084d7aa886cd15e998aa9019
franzosterkamp <franz.osterkamp@gmx.net>
2019-11-15 11:11:16 +0100

    create first api var

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "babel-polyfill";
 
-console.log("hallo");
+const searchQuery = "pizza";
+const appID = "36f30a0c";
+const apiKey = "1967e8a493e9ea463eaf1e654a98999d";
+
+const bspLink = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appID}c&app_key=${apiKey}`;
+
+console.log(bspLink);
